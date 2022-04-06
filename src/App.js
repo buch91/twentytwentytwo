3762352fa298f1f58bae043904242e780dbcabc0
ida <idabuchmadsen@gmail.com>
2022-04-06 16:43:56 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './styles.css';
+import IconBird from '../assets/images/icon-bird.jpg'
+
 const App = () => {
     return (
-      <div>
-        <p>React app</p>
-      </div>
+        <div className='reactheader'>
+          <img src={IconBird}/>
+          <p>React app</p>
+        </div>
     );
   };
   export default App;
